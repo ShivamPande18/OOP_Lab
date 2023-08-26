44780fc91ed0c1bfda61fe03c9a99552e1414194
Shivam Pande <shivshivampande18@gmail.com>
2023-08-26 14:48:48 +0530

    Create Box.java

---
+++ OOPS_LAB/Lab5/Box.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+class Box {
+    private int length;
+    private int bredth;
+    private int height;
+
+    Box()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter length:");
+        length = sc.nextInt();
+        System.out.println("Enter bredth:");
+        bredth = sc.nextInt();
+        System.out.println("Enter height:");
+        height = sc.nextInt();
+        sc.close();
+        System.out.println("Box created");
+    }
+
+    Box(int l, int b,  int h)
+    {
+        length = l;
+        bredth = b;
+        height = h;
+        System.out.println("Box created");
+    }
+
+    int getVolume()
+    {
+        return length*bredth*height;
+    }
+}
+
+class Item
+{
+    public static void main(String[] args) {
+        Box b = new Box();
+        System.out.println("Volume of the box is: " + b.getVolume());
+    }
+}
