f6451dbfd64af02fbd4e7d128c46c9600af07365
Shivam Pande <shivshivampande18@gmail.com>
2023-11-03 15:23:27 +0530

    Create Counter.java

---
+++ OOPS_LAB/Lab7/Counter.java
@@ -0,0 +1,21 @@
+class Counter {
+    private static int count = 0;
+
+    public Counter() {
+        count++;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Counter c1 = new Counter();
+        Counter c2 = new Counter();
+        Counter c3 = new Counter();
+
+        System.out.println("Number of objects created: " + Counter.getCount());
+    }
+}
